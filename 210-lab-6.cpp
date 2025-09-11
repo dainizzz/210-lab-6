adbdd4fe769ea0bdcdcd99889decbd11d5f367f3
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-09-10 23:17:49 -0700

    Initial commit

---
+++ 210-lab-6.cpp
@@ -0,0 +1,18 @@
+// COMSC-210 | Lab 6 | Dainiz Almazan
+// IDE used: CLion
+//TODO: Use pointer notation over array notation
+
+// receives a dynamic double array and populates it with values that the user enters via the console.
+void enterArrayData();
+// receives a dynamic double array and outputs its contents on one line.
+void outputArrayData();
+//  receives a dynamic double array and both calculates and returns its sum. There is no output in this function
+double sumArray();
+
+int main(){
+  // using dynamic memory allocation, create an array of doubles of size 5.
+  // Use a constant for this integer literal.
+  // Pass the array to the three functions to populate, output, and sum the array.
+
+  return 0;
+}
