d31cfd05c25893f452dccaf6eafd07adca3ae6f1
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-09-14 12:24:05 -0700

    Implements sumArray()

--- 210-lab-6.cpp
+++ 210-lab-6.cpp
@@ -1,6 +1,5 @@
 // COMSC-210 | Lab 6 | Dainiz Almazan
 // IDE used: CLion
-//TODO: Use pointer notation over array notation
 
 #include <iostream>
 using namespace std;
@@ -10,25 +9,25 @@ const int ARRAY_SIZE = 5;
 // enterArrayData() receives a dynamic double array and populates it with values that the user enters via the console.
 // arguments: a dynamic array of type double, an int representing the array's size
 // returns: nothing
-void enterArrayData(double *, const int);
+void enterArrayData(double *, int);
 
 // outputArrayData() receives a dynamic double array and outputs its contents on one line.
 // arguments: a dynamic array of type double
 // returns: nothing
-void outputArrayData(double *, const int);
+void outputArrayData(double *, int);
 
 // sumArray() receives a dynamic double array and both calculates and returns its sum.
 // arguments: a dynamic array of type double
 // returns: the sum of the values in the array
-double sumArray();
+double sumArray(double *, int);
 
 int main() {
 	double *arrptr = nullptr;
 	arrptr = new double[ARRAY_SIZE];
 
-	// Pass the array to the three functions to populate, output, and sum the array.
 	enterArrayData(arrptr, ARRAY_SIZE);
 	outputArrayData(arrptr, ARRAY_SIZE);
+	cout << "\nSum of values: " << sumArray(arrptr, ARRAY_SIZE) << endl;
 
 	delete [] arrptr;
 
@@ -45,11 +44,16 @@ void enterArrayData(double *arrptr, const int size) {
 }
 
 void outputArrayData(double *arrptr, const int size) {
-	cout << "Outputting array elements: " << endl;
+	cout << "Outputting array elements: ";
 	for (int i = 0; i < size; i++) {
-		cout << *(arrptr + i);
+		cout << *(arrptr + i) << " ";
 	}
 }
 
-double sumArray() {
+double sumArray(double *arrptr, const int size) {
+	double sum = 0;
+	for (int i = 0; i < size; i++) {
+		sum += *(arrptr + i);
+	}
+	return sum;
 }
