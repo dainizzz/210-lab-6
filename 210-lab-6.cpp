b275760f43f925b03496d57efafea0596188d636
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-09-14 11:51:15 -0700

    Starting coding session

--- 210-lab-6.cpp
+++ 210-lab-6.cpp
@@ -2,6 +2,8 @@
 // IDE used: CLion
 //TODO: Use pointer notation over array notation
 
+const int ARRAY_SIZE = 5;
+
 // enterArrayData() receives a dynamic double array and populates it with values that the user enters via the console.
 // arguments: a dynamic array of type double
 // returns: nothing
@@ -17,7 +19,7 @@ double sumArray();
 
 int main(){
   // using dynamic memory allocation, create an array of doubles of size 5.
-  // Use a constant for this integer literal.
+
   // Pass the array to the three functions to populate, output, and sum the array.
 
   return 0;
