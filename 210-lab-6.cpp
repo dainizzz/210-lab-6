d836099a7d0690b453a81fe565d996e1705a6cc8
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-09-14 12:17:27 -0700

    Implements outputArrayData()

--- 210-lab-6.cpp
+++ 210-lab-6.cpp
@@ -11,37 +11,45 @@ const int ARRAY_SIZE = 5;
 // arguments: a dynamic array of type double, an int representing the array's size
 // returns: nothing
 void enterArrayData(double *, const int);
+
 // outputArrayData() receives a dynamic double array and outputs its contents on one line.
 // arguments: a dynamic array of type double
 // returns: nothing
-void outputArrayData(double *);
+void outputArrayData(double *, const int);
+
 // sumArray() receives a dynamic double array and both calculates and returns its sum.
 // arguments: a dynamic array of type double
 // returns: the sum of the values in the array
 double sumArray();
 
-int main(){
-    double *arrptr = nullptr;
-    arrptr = new double[ARRAY_SIZE];
+int main() {
+	double *arrptr = nullptr;
+	arrptr = new double[ARRAY_SIZE];
 
-    // Pass the array to the three functions to populate, output, and sum the array.
+	// Pass the array to the three functions to populate, output, and sum the array.
+	enterArrayData(arrptr, ARRAY_SIZE);
+	outputArrayData(arrptr, ARRAY_SIZE);
 
-    delete [] arrptr;
+	delete [] arrptr;
 
-    return 0;
+	return 0;
 }
 
 void enterArrayData(double *arrptr, const int size) {
-    for (int i = 0; i < size; i++) {
-        cout << "Enter a number to add to the array:" << endl;
-        cin >> *(arrptr + i);
-    }
+	cout << "Data entry for the array:" << endl;
+	for (int i = 0; i < size; i++) {
+		cout << "\t> Element #" << i << ": ";
+		cin >> *(arrptr + i);
+	}
+	cout << "Data entry complete." << endl;
 }
 
-void outputArrayData(double *arr) {
-
+void outputArrayData(double *arrptr, const int size) {
+	cout << "Outputting array elements: " << endl;
+	for (int i = 0; i < size; i++) {
+		cout << *(arrptr + i);
+	}
 }
 
 double sumArray() {
-
 }
