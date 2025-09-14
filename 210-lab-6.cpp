5a7ca19374d5b5bcedf2a879680193f74e87712f
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-09-14 12:10:16 -0700

    Implements enterArrayData()

--- 210-lab-6.cpp
+++ 210-lab-6.cpp
@@ -2,12 +2,15 @@
 // IDE used: CLion
 //TODO: Use pointer notation over array notation
 
+#include <iostream>
+using namespace std;
+
 const int ARRAY_SIZE = 5;
 
 // enterArrayData() receives a dynamic double array and populates it with values that the user enters via the console.
-// arguments: a dynamic array of type double
+// arguments: a dynamic array of type double, an int representing the array's size
 // returns: nothing
-void enterArrayData(double *);
+void enterArrayData(double *, const int);
 // outputArrayData() receives a dynamic double array and outputs its contents on one line.
 // arguments: a dynamic array of type double
 // returns: nothing
@@ -18,18 +21,21 @@ void outputArrayData(double *);
 double sumArray();
 
 int main(){
-  double *arrptr = nullptr;
-  arrptr = new double[ARRAY_SIZE];
+    double *arrptr = nullptr;
+    arrptr = new double[ARRAY_SIZE];
 
-  // Pass the array to the three functions to populate, output, and sum the array.
+    // Pass the array to the three functions to populate, output, and sum the array.
 
-  delete [] arrptr;
+    delete [] arrptr;
 
-  return 0;
+    return 0;
 }
 
-void enterArrayData(double *arr) {
-
+void enterArrayData(double *arrptr, const int size) {
+    for (int i = 0; i < size; i++) {
+        cout << "Enter a number to add to the array:" << endl;
+        cin >> *(arrptr + i);
+    }
 }
 
 void outputArrayData(double *arr) {
